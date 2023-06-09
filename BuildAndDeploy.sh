77a456e8f9a8377726b4314a4361a47817806b98
Aaron Grode <agrode@amazon.com>
2023-06-09 13:54:21 -0500

    script update

--- BuildAndDeploy.sh
+++ BuildAndDeploy.sh
@@ -20,9 +20,39 @@ build() {
 }
 
 deploy() {
-  ls
+  cd pre-cloudfront-cognito-stack
+  cdk synth
+  cdk bootstrap
+  cdk deploy
+  cd ../
+  cdk synth
+  cdk bootstrap
   cdk deploy
 }
 
+deploySecretsOnly() {
+  cd pre-cloudfront-cognito-stack
+  cdk synth
+  cdk bootstrap
+  cdk deploy
+}
+deployMainStack() {
+  cd ../
+  cdk synth
+  cdk bootstrap
+  cdk deploy
+}
+
+awaitUserConfirmation(){
+  echo "Secrets Manager has been deployed. Please go update the values via AWS console. See documentation for more details."
+  echo ""
+while [[ ! "$input" =~ ^[Yy]$ ]]; do
+    # Prompt the user to enter "Y"
+    read -p "Please enter 'Y' to proceed ONLY AFTER you have updated Secrets Manager: " input
+done
+
+}
 build
-deploy
+deploySecretsOnly
+awaitUserConfirmation
+deployMainStack
