c23c1fc8e4093855855c98197f8a8c3042e3aa9b
Ahmed Mohamed <ahmoe@amazon.com>
2023-08-10 18:51:51 +0000

    minor secuity fixes

--- cloudfront-cognito-stack/bin/cloudfront-cognito-stack.ts
+++ cloudfront-cognito-stack/bin/cloudfront-cognito-stack.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { InfrastructureStack } from '../lib/InfrastructureStack';
-
+import { AwsSolutionsChecks } from 'cdk-nag'
+import { Aspects } from 'aws-cdk-lib';
 const app = new cdk.App();
+
+Aspects.of(app).add(new AwsSolutionsChecks({verbose:true}));
 
 new InfrastructureStack(app, 'Development-InfrastructureStack',{
     env:{
